59219349ad355cfcaa99529dce2ce37feea56c56
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-15 00:17:37 +1100

    assignment submiited

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+let element = document.createElement("h1");
+document.body.appendChild(element);
+element.id ='victory'
+
+let newHeader = document.querySelector("h1#victory");
+
+element.innerHTML ="YOUR-NAME is the champion";
